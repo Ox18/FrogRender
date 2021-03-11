4eaa6d16022b35e0eaf70178d15a5cbc8e831c2a
Alex <73305665+alexwathever@users.noreply.github.com>
2021-03-10 23:17:13 -0500

    Clean App

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,24 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FrogRender;
+using System.IO;
 using EntityLayer;
 using System.Drawing;
 
 
-namespace ConsoleApp1
+namespace App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ImgEL imgEL = new ImgEL(@"C:\Users\Wilmer\Desktop\mh00008.img");
-            imgEL = ImgFR.ImportImg(imgEL);
-            SpriteEL spriteEL = new SpriteEL(ImgFR.getImages(imgEL));
-            Bitmap bitmap = SpriteFR.getPrintHorizontal(spriteEL);
-            bitmap.Save(@"C:\Users\Wilmer\Desktop\mh00008.png");
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Please, insert the path of file img:");
+                    string directoryImg = Console.ReadLine();
+
+                    // Instance of Img
+                    Console.WriteLine("Creating IMG object...");
+                    ImgEL imgEL = new ImgEL(directoryImg);
+
+                    // Importing Img
+                    Console.WriteLine("Importing...");
+                    imgEL = ImgFR.ImportImg(imgEL);
+
+                    // Read all images
+                    Console.WriteLine("Read all images...");
+                    SpriteEL spriteEL = new SpriteEL(ImgFR.getImages(imgEL));
+
+                    // Rendering images horizontally
+                    Console.WriteLine("Rendering images...");
+                    Bitmap bitmap = SpriteFR.getPrintHorizontal(spriteEL);
+
+                    // Save image
+                    Console.WriteLine("Please, enter the folder where the image will be saved:");
+                    string directorySave = Console.ReadLine();
+                    bitmap.Save(Path.Combine(directorySave, "output.png"));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Sorry, an error occurred");
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
